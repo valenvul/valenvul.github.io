134eff60cdd72b56820a4efd3798d6ad3781b309
valentina vul <valenvul@192.168.0.110>
2022-10-21 14:31:35 -0300

    listo

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+const valorTicket = 200;
+
 function calcular(){
     cantidad = document.getElementById("cantidad").value;
     
     var select = document.getElementById("categoria");
     var valorSel = select.options[select.selectedIndex].value;
 
-    res = 200 * cantidad;
+    valorTotal = valorTicket * cantidad;
 
     switch(valorSel = parseInt(valorSel)){
+        case 0:
+            res = valorTotal;
+            break;
         case 1:
-            res = res * 0.2;
+            res = valorTotal * 0.2;
             break;
         case 2:
-            res = res * 0.5;
+            res = valorTotal * 0.5;
             break;
         case 3:
-            res = res * 0.85;
+            res = valorTotal * 0.85;
             break;
     }
 
